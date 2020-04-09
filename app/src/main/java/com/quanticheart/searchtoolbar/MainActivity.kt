f9cfa5c023372e0069e2e8f2d2278c5ebf06a9c6
jonny <dev.jonny255d@gmail.com>
2020-04-09 18:51:54 -0300

    Finish create firsts functions for searchBar

--- app/src/main/java/com/quanticheart/searchtoolbar/MainActivity.kt
+++ app/src/main/java/com/quanticheart/searchtoolbar/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.quanticheart.searchtoolbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchToolbar.setMenuOnClickListener {
+            when (it) {
+                R.id.act1 -> {
+                    Toast.makeText(this, "ACT 1", Toast.LENGTH_SHORT).show()
+                }
+                R.id.act2 -> {
+                    Toast.makeText(this, "ACT 2", Toast.LENGTH_SHORT).show()
+                }
+                R.id.act3 -> {
+                    Toast.makeText(this, "ACT 3", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        searchToolbar.setSearchTextListener {
+            textSearch.text = it
+        }
     }
 }
--- searchbar/src/main/java/com/quanticheart/searchbar/SearchBar.kt
+++ searchbar/src/main/java/com/quanticheart/searchbar/SearchBar.kt
@@ -1,14 +1,23 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
 package com.quanticheart.searchbar
 
 import android.animation.Animator
 import android.animation.Animator.AnimatorListener
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewAnimationUtils
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.FrameLayout
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.m_toobar.view.*
 import kotlin.math.hypot
 
@@ -18,62 +27,165 @@ class SearchBar @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
     private var mInflater: LayoutInflater? = null
-//    private var menu: MenuItem
+    /**
+     * EditText
+     */
+    /**
+     * Menu
+     */
+    private var textChangeClickListener: ((String) -> Unit)? = null
+    private var menuItemClickListener: ((Int) -> Unit)? = null
+    private var callbackBackClickListener: (() -> Unit)? = null
+    private var menuRes: Int? = null
+
+    /**
+     * Menu Actions Icons
+     */
+    private var iconSearch = R.drawable.m_search_bar_icon
+    private var iconSearchClose = R.drawable.ic_close_searchbar
 
     init {
         mInflater = LayoutInflater.from(context)
         mInflater?.inflate(R.layout.m_toobar, this, true)
 
-//        attrs?.let {
-//            val typedArray = context.obtainStyledAttributes(attrs, R.styleable.MSeekBar)
-//            if (typedArray.hasValue(R.styleable.MSeekBar_showPim)) {
-//                showPim = typedArray.getBoolean(R.styleable.MSeekBar_showPim, false)
-//                showPim(showPim)
-//            }
-//
-//            if (typedArray.hasValue(R.styleable.MSeekBar_showPimOnTouch)) {
-//                showPimOnTouch = typedArray.getBoolean(R.styleable.MSeekBar_showPimOnTouch, false)
-//            }
-//
-//            if (typedArray.hasValue(R.styleable.MSeekBar_progressSeekbar)) {
-//                val progressBase = typedArray.getInt(R.styleable.MSeekBar_progressSeekbar, 0)
-//                val progress = if (progressBase > 100) 100 else if (progressBase < 0) 0 else progressBase
-//                setProgress(progress)
-//            }
-//            typedArray.recycle()
-//        }
+        /**
+         * Attrs
+         */
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(attrs, R.styleable.SearchBar)
+            if (typedArray.hasValue(R.styleable.SearchBar_addMenu)) {
+                menuRes = typedArray.getResourceId(R.styleable.SearchBar_addMenu, 0)
+            }
 
-        menuToolbarSearch.apply {
-            inflateMenu(R.menu.m_menu_search_toolbar)
-            setOnMenuItemClickListener {
-                when (it.itemId) {
-                    R.id.action_search -> {
-                        Toast.makeText(context, "DEU CERTO", Toast.LENGTH_LONG).show()
-                    }
-                    else -> {
+            if (typedArray.hasValue(R.styleable.SearchBar_iconSearch)) {
+                iconSearch = typedArray.getResourceId(R.styleable.SearchBar_iconSearch, 0)
+            }
+
+            if (typedArray.hasValue(R.styleable.SearchBar_iconClose)) {
+                iconSearchClose = typedArray.getResourceId(R.styleable.SearchBar_iconClose, 0)
+            }
+
+            if (typedArray.hasValue(R.styleable.SearchBar_searchHint)) {
+                mSearchEditText.hint = typedArray.getString(R.styleable.SearchBar_searchHint)
+            }
+            typedArray.recycle()
+        }
+
+        /**
+         * Menu
+         */
+
+        menuRes?.let { res ->
+            if (res != 0) {
+                menuToolbarSearch.apply {
+                    inflateMenu(res)
+                    setOnMenuItemClickListener {
+                        menuItemClickListener?.let { it1 -> it1(it.itemId) }
+                        true
                     }
                 }
-                true
             }
         }
 
-        menuSearchClose.setOnClickListener {
-            Toast.makeText(context, "DEU CERTO CLOSE", Toast.LENGTH_LONG).show()
+        menuSearch.setImageDrawable(context.getDrawable(iconSearchClose))
+        menuSearch.setImageDrawable(context.getDrawable(iconSearch))
+
+        /**
+         * Search Text
+         */
+
+        mSearchEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        /**
+         * Search Go
+         */
+
+        menuSearchGo.setOnClickListener {
+            textChangeClickListener?.let { it(mSearchEditText.text.toString()) }
+            closeSearch()
         }
+
+        /**
+         * Menu Icon Actions
+         */
 
         menuSearch.setOnClickListener {
             searchMenu()
         }
 
+        /**
+         * Edittext
+         */
+
+        mSearchEditText.keyActionDoneListener {
+            textChangeClickListener?.let { it(mSearchEditText.text.toString()) }
+            closeSearch()
+        }
+
+        menuToolbarSearch.title = "Testes"
+//        menuToolbarSearch.navigationIcon = context.resources.getDrawable(R.drawable.m_ic_back, null)
+        menuToolbarSearch.setNavigationOnClickListener { callbackBackClickListener?.let { it1 -> it1() } }
     }
 
+    private fun toolbarAction(
+        toolbar: Toolbar,
+        context: Context,
+        toolbarTitle: String?
+    ) {
+        val bar = toolbar
+        toolbarTitle?.let {
+            bar?.title = it
+        } ?: run { }
+
+        toolbar.navigationIcon = context.resources.getDrawable(R.drawable.m_ic_back, null)
+        toolbar.setNavigationOnClickListener { callbackBackClickListener?.let { it1 -> it1() } }
+    }
+
+    fun setBackClickListener(callback: () -> Unit) {
+        callbackBackClickListener = callback
+    }
+
+    fun setMenuOnClickListener(callback: (Int) -> Unit) {
+        menuItemClickListener = callback
+    }
+
+    fun setSearchTextListener(callback: (String) -> Unit) {
+        textChangeClickListener = callback
+    }
+
+    private fun EditText?.keyActionDoneListener(action: () -> Unit) {
+        this?.setOnEditorActionListener { _: TextView?, actionId: Int, event: KeyEvent? ->
+            if (event != null && event.keyCode == KeyEvent.KEYCODE_ENTER || actionId == EditorInfo.IME_ACTION_SEARCH) {
+                action()
+            }
+            true
+        }
+    }
+
+    /**
+     * Menu Actions, show and hide
+     */
     private var searchStatus = false
 
-    private fun searchMenu() = if (searchStatus) searchClose() else searchOpen()
+    private fun searchMenu() = if (searchStatus) closeSearch() else openSearch()
 
-    private fun searchOpen() {
+    fun openSearch() {
         menuSearch.isEnabled = false
-        menuSearch.setImageDrawable(context.getDrawable(R.drawable.ic_close_searchbar))
+        menuSearch.setImageDrawable(context.getDrawable(iconSearchClose))
+        mSearchEditText.apply {
+            requestFocus()
+            openKeyboard()
+        }
 
         val x: Int = menuSearchCenter.x.toInt()
         val y: Int = menuSearchCenter.y.toInt()
@@ -82,11 +194,7 @@ class SearchBar @JvmOverloads constructor(
         val endRadius = hypot(width.toDouble(), height.toDouble()).toInt()
 
         val anim = ViewAnimationUtils.createCircularReveal(
-            mSearchToolbarLayout,
-            x,
-            y,
-            startRadius.toFloat(),
-            endRadius.toFloat()
+            mSearchToolbarLayout, x, y, startRadius.toFloat(), endRadius.toFloat()
         )
 
         anim.addListener(object : AnimatorListener {
@@ -99,16 +207,19 @@ class SearchBar @JvmOverloads constructor(
             override fun onAnimationCancel(animator: Animator) {}
             override fun onAnimationRepeat(animator: Animator) {}
         })
-
         mSearchToolbarLayout.visibility = View.VISIBLE
         anim.start()
         searchStatus = true
     }
 
-    private fun searchClose() {
+    fun closeSearch() {
         menuSearch.isEnabled = false
         mSearchToolbar.visibility = View.VISIBLE
-        menuSearch.setImageDrawable(context.getDrawable(R.drawable.m_search_bar_icon))
+        menuSearch.setImageDrawable(context.getDrawable(iconSearch))
+        mSearchEditText.apply {
+            clearFocus()
+            closeKeyboard()
+        }
 
         val x: Int = menuSearchCenter.x.toInt()
         val y: Int = menuSearchCenter.y.toInt()
@@ -117,12 +228,9 @@ class SearchBar @JvmOverloads constructor(
         val endRadius = 0
 
         val anim = ViewAnimationUtils.createCircularReveal(
-            mSearchToolbarLayout,
-            x,
-            y,
-            startRadius.toFloat(),
-            endRadius.toFloat()
+            mSearchToolbarLayout, x, y, startRadius.toFloat(), endRadius.toFloat()
         )
+
         anim.addListener(object : AnimatorListener {
             override fun onAnimationStart(animator: Animator) {}
             override fun onAnimationEnd(animator: Animator) {
@@ -135,5 +243,15 @@ class SearchBar @JvmOverloads constructor(
         })
         anim.start()
         searchStatus = false
+    }
+
+    private fun EditText.openKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun EditText.closeKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.hideSoftInputFromWindow(this.windowToken, 0)
     }
 }
